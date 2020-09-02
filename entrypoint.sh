dea6ac3a7e6c57ef42e51c32f7a1cc81aa4d2a2a
saad <saad.mk112@gmail.com>
2020-09-02 18:47:32 +0600

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 git config user.name ${USERNAME}
 git config user.email ${EMAIL}
-
+echo "START"
 python changelog-ci.py
-
+echo "END"
 git add .
 git commit -m "Added Changelog"
 git push origin ${GITHUB_REF}
