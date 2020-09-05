884cb18287755c3182f58546e27b53b968d91911
saad <saad.mk112@gmail.com>
2020-09-05 17:00:51 +0600

    cleanup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,7 +7,7 @@ git fetch --prune --unshallow
 git checkout ${GITHUB_HEAD_REF}
 
 python /scripts/changelog-ci.py
-echo ${INPUT_CHANGELOG_FILENAME}
+
 git add ${INPUT_CHANGELOG_FILENAME}
 git commit -m "Added Changelog"
 git push -u --force origin HEAD:${GITHUB_HEAD_REF}
