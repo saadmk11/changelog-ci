e919ddda4c772c544ca8b17ab37384b88ca0e8eb
saad <saad.mk112@gmail.com>
2020-09-09 14:41:24 +0600

    Add Comment support

--- scripts/changelog-ci.py
+++ scripts/changelog-ci.py
@@ -20,24 +20,29 @@ DEFAULT_GROUP_CONFIG = []
 class ChangelogCI:
 
     def __init__(
-        self, repository,
-        event_path, filename='CHANGELOG.md',
-        config_file=None, token=None
+            self, repository,
+            event_path, filename='CHANGELOG.md',
+            config_file=None, token=None
     ):
         self.repository = repository
-        self.pull_request_title = self._pull_request_title(event_path)
         self.filename = filename
         self.config = self._parse_config(config_file)
         self.token = token
+
+        title, number = self._get_pull_request_title_and_number(event_path)
+        self.pull_request_title = title
+        self.pull_request_number = number
 
     @staticmethod
     def _default_config():
         """Default configuration for Changelog CI"""
         return {
             "header_prefix": DEFAULT_VERSION_PREFIX,
+            "commit_changelog": True,
+            "comment_changelog": False,
             "pull_request_title_regex": DEFAULT_PULL_REQUEST_TITLE_REGEX,
             "version_regex": DEFAULT_SEMVER_REGEX,
-            "group_config": DEFAULT_GROUP_CONFIG
+            "group_config": DEFAULT_GROUP_CONFIG,
         }
 
     @staticmethod
@@ -50,14 +55,15 @@ class ChangelogCI:
         )
 
     @staticmethod
-    def _pull_request_title(event_path):
+    def _get_pull_request_title_and_number(event_path):
         """Gets pull request title from ``GITHUB_EVENT_PATH``"""
         with open(event_path, 'r') as json_file:
             # This is just a webhook payload available to the Action
             data = json.load(json_file)
             title = data["pull_request"]['title']
+            number = data['number']
 
-        return title
+        return title, number
 
     def _parse_config(self, config_file):
         """parse the config file if not provided use default config"""
@@ -204,50 +210,106 @@ class ChangelogCI:
 
         return items
 
-    def _parse_data(self, pull_request_data):
+    def _parse_data(self, version, pull_request_data):
         """Parse the pull requests data and return a writable data structure"""
-        data = []
+        string_data = (
+                '## ' + self.config['header_prefix'] + ' ' + version + '\n\n'
+        )
+
         group_config = self.config['group_config']
 
         if group_config:
             for config in group_config:
-                title = '#### ' + config['title'] + '\n\n'
-                items = []
+
+                if len(pull_request_data) == 0:
+                    break
+
+                items_string = ''
 
                 for pull_request in pull_request_data:
                     # check if the pull request label matches with
                     # any label of the config
                     if (
-                        any(
-                            label in pull_request['labels']
-                            for label in config['labels']
-                        )
+                            any(
+                                label in pull_request['labels']
+                                for label in config['labels']
+                            )
                     ):
-                        items.append(self._get_changelog_line(pull_request))
+                        items_string += self._get_changelog_line(pull_request)
                         # remove the item so that one item
                         # does not match multiple groups
                         pull_request_data.remove(pull_request)
 
-                data.append({'title': title, 'items': items})
+                if items_string:
+                    string_data += '\n#### ' + config['title'] + '\n\n'
+                    string_data += '\n' + items_string
 
             if pull_request_data:
+                # if they do not match any user provided group
                 # Add items in ``Other Changes`` group
-                # if they do not match any provided group
-                title = '#### Other Changes' + '\n\n'
-                items = map(self._get_changelog_line, pull_request_data)
-
-                data.append({'title': title, 'items': items})
+                string_data += '\n#### Other Changes\n\n'
+                string_data += ''.join(map(self._get_changelog_line, pull_request_data))
         else:
             # If group config does not exist then append it without and groups
-            title = ''
-            items = map(self._get_changelog_line, pull_request_data)
+            string_data += ''.join(map(self._get_changelog_line, pull_request_data))
 
-            data.append({'title': title, 'items': items})
+        return string_data
 
-        return data
-
-    def write_changelog(self):
+    def _write_changelog(self, string_data):
         """Write changelog to the changelog file"""
+        file_mode = self._get_file_mode()
+
+        with open(self.filename, file_mode) as f:
+            # read the existing data and store it in a variable
+            body = f.read()
+            # write at the top of the file
+            f.seek(0, 0)
+            f.write(string_data)
+
+            if body:
+                # re-write the existing data
+                f.write('\n\n')
+                f.write(body)
+
+    def _comment_changelog(self, string_data):
+        """Comments Changelog to the pull request"""
+        owner, repo = self.repository.split('/')
+
+        payload = {
+            'owner': owner,
+            'repo': repo,
+            'issue_number': self.pull_request_number,
+            'body': string_data
+        }
+        print(payload)
+
+        url = (
+            'https://api.github.com/repos/{repo}/issues/{number}/comments'
+        ).format(
+            repo=self.repository,
+            number=self.pull_request_number
+        )
+
+        response = requests.post(
+            url, headers=self._get_request_headers(),
+            data=payload
+        )
+        print(response.status_code)
+        print(response.json())
+
+    def run(self):
+        if (
+                not self.config['commit_changelog'] and
+                not self.config['comment_changelog']
+        ):
+            logger.warning(
+                'Skipping Changelog generation as both ``commit_changelog`` '
+                'and ``comment_changelog`` is set to False. '
+                'If you did not intend to do this please set '
+                'one or both of them to True.'
+            )
+            return
+
         is_valid_pull_request = self._validate_pull_request()
 
         if not is_valid_pull_request:
@@ -279,37 +341,13 @@ class ChangelogCI:
         if not pull_request_data:
             return
 
-        file_mode = self._get_file_mode()
-        data_to_write = self._parse_data(pull_request_data)
+        string_data = self._parse_data(version, pull_request_data)
 
-        with open(self.filename, file_mode) as f:
-            # read the existing data and store it in a variable
-            body = f.read()
-            # get the version header prefix from the config
-            version = self.config['header_prefix'] + ' ' + version
+        if self.config['commit_changelog']:
+            self._write_changelog(string_data)
 
-            # write at the top of the file
-            f.seek(0, 0)
-            f.write(version + '\n')
-            f.write('=' * len(version))
-            f.write('\n')
-
-            for data in data_to_write:
-                title = data['title']
-                items = data['items']
-
-                # Only write title if data contains items
-                if title and items:
-                    f.write('\n')
-                    f.write(title)
-
-                f.write('\n')
-                f.writelines(items)
-
-            if body:
-                # re-write the existing data
-                f.write('\n\n')
-                f.write(body)
+        if self.config['comment_changelog']:
+            self._comment_changelog(string_data)
 
 
 def parse_config(config):
@@ -360,6 +398,20 @@ def parse_config(config):
             "version_regex": DEFAULT_SEMVER_REGEX
         })
 
+    commit_changelog = config.get('commit_changelog')
+    comment_changelog = config.get('comment_changelog')
+
+    if not commit_changelog:
+        commit_changelog = True
+
+    if not comment_changelog:
+        comment_changelog = False
+
+    config.update({
+        "commit_changelog": bool(commit_changelog),
+        "comment_changelog": bool(comment_changelog)
+    })
+
     header_prefix = config.get('header_prefix')
     group_config = config.get('group_config')
 
@@ -377,7 +429,7 @@ def parse_config(config):
     if not group_config or not isinstance(group_config, list):
         logger.warning(
             '``group_config`` was not provided or not valid '
-            'Falling back to default group config.'
+            'Falling back to default regex.'
         )
         # if the group_config is not not available
         # fallback to default group_config
@@ -385,35 +437,24 @@ def parse_config(config):
             "group_config": DEFAULT_GROUP_CONFIG
         })
     else:
-        try:
-            # Check if all the group configs match the schema
-            for config in group_config:
-                if not isinstance(config, dict):
-                    raise TypeError(
-                        'group_config items must have key, '
-                        'value pairs of title and labels'
-                    )
-                title = config.get('title')
-                labels = config.get('labels')
+        # Check if all the group configs match the schema
+        for config in group_config:
+            if not isinstance(config, dict):
+                raise TypeError(
+                    'group_config items must have key, '
+                    'value pairs of title and labels'
+                )
+            title = config.get('title')
+            labels = config.get('labels')
 
-                if not title:
-                    raise KeyError('group_config item must contain title')
+            if not title:
+                raise KeyError('group_config item must contain title')
 
-                if not labels:
-                    raise KeyError('group_config item must contain labels')
+            if not labels:
+                raise KeyError('group_config item must contain labels')
 
-                if not isinstance(labels, list):
-                    raise TypeError('group_config labels must be an Array')
-
-        except Exception as e:
-            logger.warning(
-                'An error occurred while parsing ``group_config``. Error: %s'
-                'Falling back to default group config.', e
-            )
-            # Fallback to default group_config
-            config.update({
-                "group_config": DEFAULT_GROUP_CONFIG
-            })
+            if not isinstance(labels, list):
+                raise TypeError('group_config labels must be an Array')
 
 
 if __name__ == '__main__':
@@ -432,4 +473,4 @@ if __name__ == '__main__':
         repository, event_path, filename=filename,
         config_file=config_file, token=token
     )
-    ci.write_changelog()
+    ci.run()
