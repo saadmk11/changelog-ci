bade602e149c1f5366728affbad0176ffe2b37a7
saadmk11 <saad.mk112@gmail.com>
2022-07-31 14:18:09 +0600

    Capture output of git commands

--- scripts/run_git.py
+++ scripts/run_git.py
@@ -8,10 +8,10 @@ def checkout_pull_request_branch(branch_name: str) -> None:
     Checkout the PR branch and pull the latest changes.
     """
     with gha_utils.group(f"Checkout '{branch_name}' branch"):
-        subprocess.run(
+        run_subprocess_command(
             ["git", "fetch", "--prune", "--unshallow", "origin", branch_name]
         )
-        subprocess.run(["git", "checkout", branch_name])
+        run_subprocess_command(["git", "checkout", branch_name])
 
 
 def configure_git_author(username: str, email: str) -> None:
@@ -22,8 +22,8 @@ def configure_git_author(username: str, email: str) -> None:
         gha_utils.notice(f"Setting Git Commit User to '{username}'.")
         gha_utils.notice(f"Setting Git Commit email to '{email}'.")
 
-        subprocess.run(["git", "config", "user.name", username])
-        subprocess.run(["git", "config", "user.email", email])
+        run_subprocess_command(["git", "config", "user.name", username])
+        run_subprocess_command(["git", "config", "user.email", email])
 
 
 def create_new_git_branch(base_branch_name: str, new_branch_name: str) -> None:
@@ -33,8 +33,8 @@ def create_new_git_branch(base_branch_name: str, new_branch_name: str) -> None:
     with gha_utils.group(
         f"\nCreate New Branch ({base_branch_name} -> {new_branch_name})"
     ):
-        subprocess.run(["git", "checkout", base_branch_name])
-        subprocess.run(["git", "checkout", "-b", new_branch_name])
+        run_subprocess_command(["git", "checkout", base_branch_name])
+        run_subprocess_command(["git", "checkout", "-b", new_branch_name])
 
 
 def git_commit_changelog(
@@ -44,8 +44,18 @@ def git_commit_changelog(
     Commit the changelog file.
     """
     with gha_utils.group(f"Commit Changelog ({changed_file})"):
-        subprocess.run(["git", "add", changed_file])
-        subprocess.run(
+        run_subprocess_command(["git", "add", changed_file])
+        run_subprocess_command(
             ["git", "commit", f"--author={commit_author}", "-m", commit_message]
         )
-        subprocess.run(["git", "push", "-u", "origin", commit_branch_name])
+        run_subprocess_command(["git", "push", "-u", "origin", commit_branch_name])
+
+
+def run_subprocess_command(command: list) -> None:
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    if result.returncode != 0:
+        gha_utils.error(result.stderr)
+        raise SystemExit(result.returncode)
+
+    gha_utils.echo(result.stdout)
